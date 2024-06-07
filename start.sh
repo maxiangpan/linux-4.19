4017d280f67d29fc6a1e84161621350e8b862ad0
maxiangpan <1104141840@qq.com>
2024-06-08 01:40:19 +0800

    build uboot

---
+++ start.sh
@@ -0,0 +1,6 @@
+qemu-system-arm -M vexpress-a9 -m 512M \
+	-kernel linux-4.19/arch/arm/boot/zImage \
+	-dtb  linux-4.19/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
+	-nographic \
+	-append "root=/dev/mmcblk0  console=ttyAMA0" \
+	-sd a9rootfs.ext3
---
+++ u-boot-2020.10/build.sh
@@ -0,0 +1,4 @@
+#make vexpress_ca9x4_defconfig
+#make CROSS_COMPILE=arm-linux-gnueabihf- all
+#最终编译生成 elf 格式的可执行文件 u-boot 和纯二进制文件u-boot.bin，其中 QEMU 可以启动的为 elf 格式的可执行文件 u-boot 
+#
