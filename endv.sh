db061227767c102d31d07c5a8b235c9e9e64e373
maxiangpan <1104141840@qq.com>
2024-10-01 22:11:20 +0800

    更新env.sh

--- endv.sh
+++ endv.sh
@@ -13,6 +13,7 @@ gcc-arm-linux-gnueabihf \
 libssl-dev liblz4-tool genext2fs \
 libsigsegv2 m4 intltool libdrm-dev \
 curl sed make \
+python3-venv python3-pip \
 binutils build-essential gcc g++ bash patch gzip bzip2 perl tar cpio unzip rsync file bc wget \
 libncurses5 libglib2.0-dev libgtk2.0-dev libglade2-dev cvs git mercurial rsync openssh-client \
 subversion asciidoc w3m dblatex graphviz  libssl-dev texinfo fakeroot \
@@ -20,3 +21,10 @@ libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev sqlite3 libsqlite3-dev \
 openssl tcl8.6-dev tk8.6-dev libreadline-dev zlib1g-dev \
 libparse-yapp-perl default-jre patchutils swig chrpath diffstat gawk time expect-dev -y
 
+pip install Sphinx
+sudo apt-get install python3-sphinx
+pip3 install sphinx_rtd_theme==1.1.1
+
+cd qemu
+#不可使用sudo 如果是这样会引起一部分的报错
+./configure
